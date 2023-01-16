ac87065920f2f978349d7f0a65473ac389658975
Shivang Mishra <35092323+ShivangMishra@users.noreply.github.com>
2023-01-16 22:58:52 +0530

    canvas in home area

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,15 @@
+import draw from "../../canvasAnim";
+import Canvas from "../canvas/Canvas";
 import "./Home.css";
+
 export default function Home() {
   return (
     <section className="home">
+      <Canvas
+        width={window.innerWidth}
+        height={window.innerHeight}
+        draw={draw}
+      />
       <div className="hero-area">
         <div className="intro-text-zone">
           <h1 className="intro-big-heading">Hi, I'm Shivang,</h1>
