8176f8e975f611b89f8b89167157fdf94943439a
Shivang Mishra <35092323+ShivangMishra@users.noreply.github.com>
2023-01-18 22:54:23 +0530

    fixed work styling

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -39,30 +39,12 @@ export default function Work() {
       imgUrl: "https://picsum.photos/400/400",
       category: "Web Development",
     },
-    {
-      title: "XYZ",
-      description: "Aliquip nisi nostrud do exercitation tempor anim velit.",
-      imgUrl: "https://picsum.photos/400/400",
-      category: "Web Development",
-    },
-    {
-      title: "XYZ",
-      description: "Aliquip nisi nostrud do exercitation tempor anim velit.",
-      imgUrl: "https://picsum.photos/400/400",
-      category: "Web Development",
-    },
-    {
-      title: "XYZ",
-      description: "Aliquip nisi nostrud do exercitation tempor anim velit.",
-      imgUrl: "https://picsum.photos/400/400",
-      category: "Web Development",
-    },
   ];
 
   return (
     <section className="work">
       <div className="work-container">
-        <h1 className="work-heading">Some of My Work</h1>
+        <h1 className="work-heading">My Work</h1>
         <div className="work-items-container">
           {projects.map((project) => (
             // <div className="work-item"></div>
