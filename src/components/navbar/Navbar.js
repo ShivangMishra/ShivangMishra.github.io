017f47e19131a2c7511a002caa6f74f628d1c964
Shivang Mishra <35092323+ShivangMishra@users.noreply.github.com>
2023-01-21 21:06:35 +0530

    added nav hash link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,35 @@
 import "./Navbar.css";
-import { logo } from "../../assets";
-import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import { NavHashLink } from "react-router-hash-link";
+import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const { hash } = useLocation();
+  const isActive = (iHash) => hash === iHash;
+  const sections = [
+    { title: "Home", to: "#home" },
+    { title: "Work", to: "#work" },
+    { title: "About", to: "#about" },
+    { title: "Contact", to: "#contact" },
+  ];
   return (
     <nav className="navbar sticky">
       <ul className="navlist left">
-        <li>
-          <h1>About</h1>
-        </li>
-        <li>
-          <h1>Skills</h1>
-        </li>
-        <li>
-          <h1>Work</h1>
-        </li>
-        <li>
-          <h1>Contact</h1>
-        </li>
+        {sections.map((section) => (
+          <li>
+            <NavHashLink
+              className={
+                isActive(section.to)
+                  ? "link-active"
+                  : "link"
+              }
+              smooth
+              to={section.to}
+            >
+              {section.title}
+            </NavHashLink>
+          </li>
+        ))}
       </ul>
-
-      {/* <img src={logo} alt="Logo" className="nav-logo" /> */}
-
       <div className="right"></div>
     </nav>
   );
