0be29aa8b88eedfe6182f34d50d328c80f085b92
Shivang Mishra <35092323+ShivangMishra@users.noreply.github.com>
2023-02-06 12:34:43 +0530

    home fixed layout issues

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,22 +13,22 @@ export default function Home() {
       />
       {/* <div className="hero-area"> */}
       <div className="intro-text-zone">
-        <div className="glitch">
-          <h1 className="intro-big-heading">I'm Shivang Mishra</h1>
-        </div>
-        <h1 className="intro-big-heading">A Software Developer</h1>
-        {/* <p class="glitch">
-          <span aria-hidden="true">codepen</span>
-          codepen
-          <span aria-hidden="true">codepen</span>
+        {/* <div className="glitch"> */}
+        {/* <h1 className="intro-big-heading glitch">I'm Shivang Mishra</h1> */}
+        {/* <p className="glitch">
+          <span aria-hidden="true">I'm Shivang Mishra</span>
+          I'm Shivang Mishra
+          <span aria-hidden="true">I'm Shivang Mishra</span>
         </p> */}
+        {/* </div> */}
+        <h1 className="intro-big-heading">I'm Shivang Mishra</h1>
+        <h1 className="intro-big-heading">A Software Developer</h1>
         <p className="hero-sub">
           I specialise in developing Web applications and Games.
         </p>
         <br />
         <br />
         <button className="contact-btn">Contact me!</button>
-        {/* </div> */}
       </div>
     </section>
   );
