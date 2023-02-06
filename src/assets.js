236a35571d49192e421b791c941e52d5d5a47428
Shivang Mishra <35092323+ShivangMishra@users.noreply.github.com>
2023-02-06 12:35:04 +0530

    asset imports

--- src/assets.js
+++ src/assets.js
@@ -1,1 +1,4 @@
 export const logo = require("./assets/icons/logo.png");
+export const speacodeImg = require("./assets/speacode.gif");
+export const retyrnImg = require("./assets/retyrn.png");
+export const penopImg = require("./assets/penop.png");
